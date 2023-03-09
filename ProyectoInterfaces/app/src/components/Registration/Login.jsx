090100f6221d2524e94a486d94e9fadcca197d0f
jujosa09 <jujosadols@uma.es>
2023-03-09 09:42:56 +0100

    comienzo del login

--- ProyectoInterfaces/app/src/components/Registration/Login.jsx
+++ ProyectoInterfaces/app/src/components/Registration/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import NavbarIntroPage from '../IntroPage/NavbarIntroPage';
 
 export default function Login() {
     return(
         <>
+        <div id='navbarLocation'>
+            <NavbarIntroPage />
+        </div>
+        <div id='login'>
+            <h1>Accede con tu cuenta</h1>
+            <form>
+                <label>Email: </label> <input type='text' name='email'></input><br/>
+                <label>Password: </label> <input type='password' name='password'></input><br/>
+                <button>Login</button>
+            </form>
+
+            <div id='accederRegister'>
+                <label>¿No tienes cuenta todavía?</label><br/>
+                <a href='/signUp'>Registrate aquí</a>
+            </div>
+        </div>
         </>
     )
 }
