71dc2bdabf773d097d3f311f8f15fb52f44b2662
jujosa09 <jujosadols@uma.es>
2023-03-09 10:25:32 +0100

    mejorado login

--- ProyectoInterfaces/app/src/components/Registration/Login.jsx
+++ ProyectoInterfaces/app/src/components/Registration/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavbarIntroPage from '../IntroPage/NavbarIntroPage';
+import '../../assets/css/login.css'
 
 export default function Login() {
     return(
@@ -7,19 +8,22 @@ export default function Login() {
         <div id='navbarLocation'>
             <NavbarIntroPage />
         </div>
-        <div id='login'>
-            <h1>Accede con tu cuenta</h1>
-            <form>
-                <label>Email: </label> <input type='text' name='email'></input><br/>
-                <label>Password: </label> <input type='password' name='password'></input><br/>
-                <button>Login</button>
-            </form>
+        <div class="my-form bg-white">
+  <h1>Accede a tu cuenta</h1>
+  <form>
+    <div class="mb-3 mt-4">
+      <label for="exampleInputEmail1" class="form-label">Email</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    </div>
+    <div class="mb-3 mt-4">
+      <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+      <input type="password" class="form-control" id="exampleInputPassword1"/>
+    </div>
+    <button type="submit" class="btn btn-light mt-3">Login</button>
+  </form>
+  <p class="mt-4">¿No tienes cuenta todavía? <a href="#">¡Regístrate ahora!</a></p>
+</div>
 
-            <div id='accederRegister'>
-                <label>¿No tienes cuenta todavía?</label><br/>
-                <a href='/signUp'>Registrate aquí</a>
-            </div>
-        </div>
         </>
     )
 }
