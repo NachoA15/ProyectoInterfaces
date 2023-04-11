2dea11bda0aa31f665432b6aa937e5aa2d7e6aaa
jujosa09 <jujosadols@uma.es>
2023-04-11 14:12:11 +0200

    Añadido chat en server

--- ProyectoInterfaces/server/index.js
+++ ProyectoInterfaces/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 require('dotenv').config();
 const app = express();
 const cors = require('cors');
@@ -9,6 +10,7 @@ app.listen(3001, () => {
 });
 
 const mysql = require('mysql');
+const { log } = require('console');
 const db = mysql.createConnection({
 	host: process.env.HOST,
 	port: process.env.PORT,
@@ -24,6 +26,7 @@ const dbQuery = (query, req, res) => {
 		res.status(200).json(results);
 	});
 };
+
 
 /** =========================================================================
  *  Consultas de las tablas
@@ -66,4 +69,65 @@ app.post("/checkUsuario", (req, res) => {
 
 	dbQuery("SELECT * FROM USUARIO WHERE username = '" + username + "' and password = '" + password + "';", req, res);
 	console.log('OK');
+})
+
+app.post("/saveChat", (req,res) => {
+	let logChat = req.body.logChat;
+	let message = req.body.message;
+	let user = req.body.user;
+
+	rutaArchivo = `logChats/${logChat}`;
+	message = `\n${user}: ${message}`;
+
+	if(!fs.existsSync("logChats/")){
+		fs.mkdir('logChats', (err) => {
+			if(err){
+				console.log(err);
+			}else{
+				console.log("Carpeta logChats creada");
+			}
+		});
+	}
+
+	fs.appendFile(rutaArchivo, message, (err => {
+		if (err){
+			console.error("El archivo no esta creado");
+			fs.writeFile(rutaArchivo, message, (err => {
+				console.log(err);
+			}))
+		}else{
+			console.log("Se ha escrito el texto correctamente");
+			res.send("Codigo 200");
+		}
+	}))
+})
+
+app.get("/getMessages", (req, res) => {
+	let user = req.body.user;
+	let logChat = req.body.logChat;
+	rutaArchivo = `logChats/${logChat}`;
+	if(fs.existsSync(rutaArchivo)){
+		fs.readFile(rutaArchivo, 'utf-8', (err, data) => {
+			if(err){
+				console.log(err);
+				res.send("202 NOT OK");
+			}else{
+				const messages = data.split("\n");
+
+				const myMessages = messages.filter((message) => message.startsWith(`${user}:`)) 
+
+				const myMessagesWithoutUser = myMessages.map(message => message.replace(`${user}: `, ''));
+				
+				console.log(myMessagesWithoutUser);
+
+				myMessagesWithoutUser.forEach((linea, index) => {
+					console.log(`Linea ${index+1}: ${linea}`)
+				})
+				res.send("200 OK");
+			}
+		})
+	}else{
+		console.log("El archivo no existe");
+		res.send("202 NOT OK");
+	}
 })
