9c285990b28da0c105524c89b84be4864af9e7b2
Nacho_A_A <nachoalav@uma.es>
2023-04-14 14:15:08 +0200

    modified:   ProyectoInterfaces/app/src/assets/css/IntroPage.css modified:   ProyectoInterfaces/app/src/components/IntroPage/IntroPage.jsx modified:   ProyectoInterfaces/app/src/components/Profile/EditProfile.jsx modified:   ProyectoInterfaces/app/src/components/Profile/Profile.jsx new file:   ProyectoInterfaces/app/src/components/Profile/ProfileView.jsx

--- ProyectoInterfaces/app/src/components/Profile/EditProfile.jsx
+++ ProyectoInterfaces/app/src/components/Profile/EditProfile.jsx
@@ -3,12 +3,10 @@ import { useParams } from "react-router-dom";
 import {TextField} from '@mui/material'
 import NavbarProfile from "./NavbarProfile";
 
-export default function EditProfile() {
+export default function EditProfile({usuarioPerfil}) {
 
     let params = useParams();
     let username = params.username;
-
-    console.log(username);
 
     return(
         <>
@@ -24,34 +22,40 @@ export default function EditProfile() {
                             <h4 className="text-right">Editar perfil</h4>
                         </div>
                         <div className="d-flex justify-content-between align-items-center mb-3">
-                        <div className="d-flex flex-column align-items-center text-center p-3 py-5">
-                            <img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"/>
-                            <label className="control-label small" htmlFor="file_img">Cambiar foto de perfil (.jpg/.png):</label> <input type="file" name="file_img"/>
+                            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+                                <img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"/>
+                                <label className="control-label small" htmlFor="file_img">Cambiar foto de perfil (.jpg/.png):</label> <input type="file" name="file_img"/>
                             </div>
 
                         </div>
                         <div className="mt-3">
                             <div className="row">
                                 <div className="col-md-4"></div>
-                                <div className="col-md-4"><TextField id="usuario" name="nombre de usuario" label="Usuario" variant="standard" size="small"/></div>
+                                <div className="col-md-4"><TextField id="usuario" name="nombre de usuario" label="Usuario" variant="standard" size="small" value={usuarioPerfil.username}/></div>
                                 <div className="col-md-4"></div>
                             </div>
                             <br/>
                             <div className="row">
                                 <div className="col-md-4"></div>
-                                <div className="col-md-4"><TextField id="nombre" name="nombre completo" label="Nombre Completo" variant="standard" size="small"/></div>
+                                <div className="col-md-4"><TextField id="nombre" name="nombre completo" label="Nombre Completo" variant="standard" size="small" value={usuarioPerfil.nombre}/></div>
                                 <div className="col-md-4"></div>
                             </div>
                             <br/>
                             <div className="row">
                                 <div className="col-md-4"></div>
-                                <div className="col-md-4"><TextField id="email" name="correo electrónico" label="Correo Electrónico" variant="standard" size="small"/></div>
+                                <div className="col-md-4"><TextField id="localizacion" name="localizacion" label="Localizacion" variant="standard" size="small" value={usuarioPerfil.localizacion}/></div>
                                 <div className="col-md-4"></div>
                             </div>
                             <br/>
                             <div className="row">
                                 <div className="col-md-4"></div>
-                                <div className="col-md-4"><TextField id="teléfono" name="teléfono" label="Número de Teléfono" variant="standard" size="small"/></div>
+                                <div className="col-md-4"><TextField id="email" name="correo electrónico" label="Correo Electrónico" variant="standard" size="small" value={usuarioPerfil.correo}/></div>
+                                <div className="col-md-4"></div>
+                            </div>
+                            <br/>
+                            <div className="row">
+                                <div className="col-md-4"></div>
+                                <div className="col-md-4"><TextField id="teléfono" name="teléfono" label="Número de Teléfono" variant="standard" size="small" value={usuarioPerfil.telefono}/></div>
                                 <div className="col-md-4"></div>
                             </div>
                             <br/>
